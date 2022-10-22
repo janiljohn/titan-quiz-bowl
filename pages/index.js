325a47e8393d2a30e89218603367cbbc000d0846
JonAguirre0 <jaguirrecsu@gmail.com>
2022-10-22 09:14:19 -0700

    Created questions for each category

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to Game!
+          Welcome to Titan Quiz Bowl!
         </h1>
 
         <div className={styles.grid}>
